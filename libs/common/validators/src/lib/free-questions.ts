da09eba4da0ffe2095a211464bf4919e2532583e
Bhagya Mudgal <bhagyamudgal@gmail.com>
2024-09-17 15:45:05 +0800

    fix: free question handle validations

--- libs/common/validators/src/lib/free-questions.ts
+++ libs/common/validators/src/lib/free-questions.ts
@@ -12,13 +12,15 @@ export const addFreeQuestionsSchema = z.object({
             required_error: "Creator handle is required",
             invalid_type_error: "Creator handle must be a string",
         })
-        .min(3, { message: "Creator handle must be at least 3 characters" }),
+        .min(3, { message: "Creator handle must be at least 3 characters" })
+        .transform((value) => value.replace(/^@+/, "")),
     userHandle: z
         .string({
             required_error: "Your handle is required",
             invalid_type_error: "Your handle must be a string",
         })
-        .min(3, { message: "Your handle must be at least 3 characters" }),
+        .min(3, { message: "Your handle must be at least 3 characters" })
+        .transform((value) => value.replace(/^@+/, "")),
 });
 
 export type AddFreeQuestionSchema = z.infer<typeof addFreeQuestionsSchema>;
