a17ba5c68dae5e0a82020a61539db41b8337df69
Bhagya Mudgal <bhagyamudgal@gmail.com>
2024-10-16 13:32:16 +0530

    style: add loading spinner to page loading

--- libs/web/ui/src/lib/common/PageLoading.tsx
+++ libs/web/ui/src/lib/common/PageLoading.tsx
@@ -1,10 +1,20 @@
 "use client";
 
+import { LoadingSpinner } from "@dansr/common-ui";
 import { Logo } from "@dansr/common-ui/server";
+import { useIsClient } from "usehooks-ts";
 
 export function PageLoading() {
+    const isClient = useIsClient();
     return (
-        <div className="fixed z-50 inset-0 flex justify-center items-center">
+        <div className="fixed z-50 inset-0 flex flex-col justify-center items-center gap-4">
+            <div
+                style={{
+                    visibility: isClient ? "visible" : "hidden",
+                }}
+            >
+                <LoadingSpinner />
+            </div>
             <Logo />
         </div>
     );
